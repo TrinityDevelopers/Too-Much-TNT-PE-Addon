c896ca7f92edd3d00bfb033993a19d03b07c97eb
TrinityDevelopers <trinitydevelopers4christ@gmail.com>
2016-01-12 14:25:40 -0600

    Add features!

--- jni/mcpe/i18n/I18n.h
+++ jni/mcpe/i18n/I18n.h
@@ -2,7 +2,14 @@
 
 #include <string>
 
+#include "Localization.h"
+
 class I18n {
 public:
+	static Localization mCurrentLanguage;
+	
 	static std::string get(const std::string&);
+	static Localization getCurrentLanguage();
+	static Localization getLocaleFor(const std::string&);
+	static std::string getCurrentLangString();
 };
